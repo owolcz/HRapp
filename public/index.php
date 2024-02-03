90ed4f21695db7b983e8bb9fc807e2240034413b
owolcz <wolczoskar@gmail.com>
2024-02-03 16:03:19 +0100

    Basic layout for home page

--- public/index.php
+++ public/index.php
@@ -1,2 +1,24 @@
-<?php
-echo "Hello, World!!!!";
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <title>HR APP</title>
+</head>
+<body>
+<h1>Strona powitalna</h1>
+<p>Zarejestruj się</p>
+<div>
+    <form>
+        <label for="email">E-mail</label> <br>
+        <input type="email" id="email" name="email"><br>
+
+        <label for="password">Hasło</label><br>
+        <input type="password" id="password" name="password"><br>
+
+        <label for="password-repeat">Powtórz hasło</label><br>
+        <input type="password" id="password-repeat" name="password-repeat"><br>
+
+        <button>Rejestracja</button>
+    </form>
+</div>
+</body>
+</html>
